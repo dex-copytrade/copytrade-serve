5c476ca587b4ce4c92095e3d06ee69c553331a3e
dream.huang <dream.huang@yijinin.com>
2022-08-29 01:16:28 +0800

    feat: add

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -1,14 +1,16 @@
 import { Controller } from "egg";
+import { getMoney } from "../utils";
+
 export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
-    await ctx.render('home.ejs');
+    await ctx.render("home.ejs");
   }
 
   public async info() {
     const { ctx } = this;
 
     const data = await ctx.service.info.info();
-    await ctx.render('info.ejs', { data });
+    await ctx.render("info.ejs", { data: { getMoney, ...data } });
   }
 }
