34c97605768576147b3e4091667d3572405db89d
dream.huang <dream.huang@yijinin.com>
2022-09-01 15:43:46 +0800

    feat: add

--- app/service/info.ts
+++ app/service/info.ts
@@ -13,13 +13,16 @@ export default class Info extends Service {
     const { ctx } = this;
     const count =  await ctx.model.TrackingAccount.find({}).count()
     const count1 =  await ctx.model.TrackingAccount.find({grasp: 1}).count()
+    const count4 =  await ctx.model.TrackingAccount.find({grasp: 2}).count()
 
     const count2 = await ctx.model.AccountTradeHistory.find({}).count();
+    const count3 = await ctx.model.SettlePerp.find({}).count();
     return {
       count,
       count1,
-      count2
-
+      count2,
+      count3,
+      count4
     }
   }
 }
