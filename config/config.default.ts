8af774efc0980fdd59655814770084f0edbe10c0
jeremy.su <jeremy.su@yijinin.com>
2022-09-05 21:40:36 +0800

    fix: fix error

--- config/config.default.ts
+++ config/config.default.ts
@@ -1,9 +1,9 @@
-import { Context, EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+import { Context, EggAppConfig, EggAppInfo, PowerPartial } from "egg";
 
 export default (appInfo: EggAppInfo) => {
   const config = {} as PowerPartial<EggAppConfig>;
 
-  (config.security = {
+  config.security = {
     csrf: {
       // ignoreJSON: true, // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
       enable: false,
@@ -12,14 +12,14 @@ export default (appInfo: EggAppInfo) => {
 
   config.view = {
     mapping: {
-      '.nj': 'nunjucks',
-      '.ejs': 'ejs',
+      ".nj": "nunjucks",
+      ".ejs": "ejs",
     },
   };
 
   config.mongoose = {
     client: {
-      url: 'mongodb://CopytradeTestUser:qazwsxrfv#$hjdsweyuuwei@43.134.151.23:27017/CopytradeTest',
+      url: "mongodb://CopytradeTestUser:qazwsxrfv#$hjdsweyuuwei@43.134.151.23:27017/CopytradeTest",
       options: {
         autoReconnect: true,
         reconnectTries: Number.MAX_VALUE,
@@ -33,7 +33,7 @@ export default (appInfo: EggAppInfo) => {
   config.keys = appInfo.name + "_1661496474724_8957";
 
   // add your egg config in here
-  config.middleware = ['responseTime', 'errorHandler', 'error'];
+  config.middleware = ["responseTime", "errorHandler", "error"];
 
   // 配置不生效
   // config.responseTime = {
@@ -58,8 +58,8 @@ export default (appInfo: EggAppInfo) => {
   };
 
   config.cors = {
-    origin: (ctx: Context) => ctx.get('origin'),
-    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+    origin: (ctx: Context) => ctx.get("origin"),
+    allowMethods: "GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS",
     credentials: true,
   };
 
